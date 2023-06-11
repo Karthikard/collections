eaba3ae1699cafb9569f1ad441cfba19b310ff8c
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-06-11 09:05:47 +0530

    Create linked_list.java

---
+++ linked_list.java
@@ -0,0 +1,17 @@
+
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	   LinkedList <String> li=new LinkedList<String>();
+	   li.add("RD");
+	   li.add("Karthika");
+	   li.add("a");
+	   
+	   li.add(2,"A");
+	   System.out.println(li);
+	   
+	   
+	}
+}
