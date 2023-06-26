1833640bdd757bd62e6022cdff1b2dd7ec8140d4
karthika RD <105119562+Karthikard@users.noreply.github.com>
2023-06-26 22:24:55 +0530

    Update hash_map1.java

--- hash_map1.java
+++ hash_map1.java
@@ -1,1 +1,17 @@
 
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		HashMap<Integer,String> hm = new HashMap<>();
+		hm.put(1,"rd");
+		hm.put(2,"karthika");
+		
+	    System.out.println(hm.get(1));    // output : rd
+	    
+	    System.out.println(hm.containsKey(1));     //output : true
+	    
+	    System.out.println(hm.size());    //output : 2
+	}
+}
+
